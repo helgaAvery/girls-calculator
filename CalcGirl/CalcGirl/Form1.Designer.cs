6e5c633934f7b62975613ed3ebaedff14b0c4fd8
helgaAvery <olelusik_3@mail.ru>
2016-02-16 14:41:20 +0600

    Add + multyply

--- CalcGirl/CalcGirl/Form1.Designer.cs
+++ CalcGirl/CalcGirl/Form1.Designer.cs
@@ -28,12 +28,120 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.textBox3);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.button12);
+            this.panel1.Controls.Add(this.button11);
+            this.panel1.Location = new System.Drawing.Point(13, 13);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(778, 635);
+            this.panel1.TabIndex = 0;
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(461, 268);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(73, 134);
+            this.button11.TabIndex = 11;
+            this.button11.Text = "*";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.multyply);
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(646, 268);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(73, 134);
+            this.button12.TabIndex = 12;
+            this.button12.Text = "+";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.add);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(15, 30);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(181, 20);
+            this.textBox1.TabIndex = 13;
+            this.textBox1.TextChanged += new System.EventHandler(this.textOne);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(212, 30);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(181, 20);
+            this.textBox2.TabIndex = 14;
+            this.textBox2.TextChanged += new System.EventHandler(this.textThree);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(399, 30);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(181, 20);
+            this.textBox3.TabIndex = 15;
+            this.textBox3.TextChanged += new System.EventHandler(this.textTwo);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(461, 408);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(73, 134);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "/";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.divide);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(646, 408);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(73, 134);
+            this.button2.TabIndex = 17;
+            this.button2.Text = "-";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.TextChanged += new System.EventHandler(this.minus);
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(803, 660);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
--- CalcGirl/CalcGirl/Form1.cs
+++ CalcGirl/CalcGirl/Form1.cs
@@ -16,5 +16,69 @@ namespace CalcGirl
         {
             InitializeComponent();
         }
+
+        private void add(object sender, EventArgs e)
+        {
+            string first = textBox1.Text;
+            string second = textBox2.Text;
+            int firstInt = Convert.ToInt32(first);
+            int secondInt = Convert.ToInt32(second);
+            double result = firstInt + secondInt;
+            textBox3.Text = result.ToString();
+
+        }
+
+        private void multyply(object sender, EventArgs e)
+        {
+            string first = textBox1.Text;
+            string second = textBox2.Text;
+            int firstInt = Convert.ToInt32(first);
+            int secondInt = Convert.ToInt32(second);
+            double result = firstInt * secondInt;
+            textBox3.Text = result.ToString();
+        }
+
+        private void textOne(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textTwo(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textThree(object sender, EventArgs e)
+        {
+
+        }
+
+        private void minus(object sender, EventArgs e)
+        {
+            string first = textBox1.Text;
+            string second = textBox2.Text;
+            float firstInt = Convert.ToInt32(first);
+            float secondInt = Convert.ToInt32(second);
+            double result = firstInt - secondInt;
+            textBox3.Text = result.ToString();
+        }
+
+        private void divide(object sender, EventArgs e)
+        {
+            string first = textBox1.Text;
+            string second = textBox2.Text;
+            int firstInt = Convert.ToInt32(first);
+            int secondInt = Convert.ToInt32(second);
+            double result = firstInt / secondInt;
+            textBox3.Text = result.ToString();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+      
     }
 }
